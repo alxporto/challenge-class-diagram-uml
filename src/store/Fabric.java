eae8fc6c2518a3a4cd6bf7d5986d9403a8e32e9a
datalastix <datalastix@gmail.com>
2024-07-23 20:07:11 -0300

    refactor: update the Fabric class
    Signed-off-by: datalastix <datalastix@gmail.com>

--- src/store/Fabric.java
+++ src/store/Fabric.java
@@ -1,4 +1,36 @@
 package store;
 
+import equipment.phone.PhoneDevice;
+import equipment.music.Music;
+import equipment.browser.Browser;
+import iphone.Iphone;
+
 public class Fabric {
+    public static void main(String[] args) {
+        Iphone iphone = new Iphone();
+        Music music = (Music) iphone;
+        PhoneDevice phoneDevice = (PhoneDevice) iphone;
+        Browser browser = (Browser) iphone;
+
+        System.out.println("Music");
+        music.selectMusic();
+        music.play();
+        music.pause();
+        System.out.println(" *(ºoº)* ");
+        System.out.println(" ");
+
+        System.out.println("Phone");
+        phoneDevice.call();
+        phoneDevice.answer();
+        phoneDevice.initVoiceMail();
+        System.out.println(" *(o)* ");
+        System.out.println(" ");
+
+        System.out.println("Internet");
+        browser.page();
+        browser.addNewTab();
+        browser.updatePage();
+        System.out.println(" (.*|*.) ");
+    }
 }
+
